304167ee4f040c9c2b89e7dfe99980f7707445af
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-07-01 12:31:12 +0200

    Align the header title in TravelView.vue

--- front-end-dev/src/App.vue
+++ front-end-dev/src/App.vue
@@ -38,6 +38,8 @@ const SavedSvgIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="35" height=
   flex-direction: column;
   width: 275px;
   height: 98.5vh;
+  background-color: blueviolet;
+
 }
 
 .MetroGo {
--- front-end-dev/src/views/TravelView.vue
+++ front-end-dev/src/views/TravelView.vue
@@ -1,3 +1,33 @@
 <template>
-    <h1>Travel Page</h1>
+    <div class="background">
+        <div class="header">
+            <h1>Time to ride ! <br> Where to this time ?</h1>
+        </div>
+    </div>
+    
 </template>
+
+<script setup>
+
+</script>
+
+<style>
+    .header {
+        padding-left: 65px;
+        padding-top: 33px;
+        font-size: 26px;
+        font-weight: 700;
+        line-height: 62px;
+        letter-spacing: 1.84px;
+        font-family: 'Open Sans';
+        height: 95vh;
+
+    }
+
+    .background {
+        /* background-color: #F6F6F6; */
+        background-color: aqua;
+        width: 87vw;
+
+    }
+</style>
