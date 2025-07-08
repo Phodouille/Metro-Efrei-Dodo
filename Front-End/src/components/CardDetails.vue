e8ead3c4d54685ef6f9c3a1d6a366d03d533cbd0
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-07-08 18:42:23 +0200

    Create a detail card on click and fill with station names

--- Front-End/src/components/CardDetails.vue
+++ Front-End/src/components/CardDetails.vue
@@ -1,90 +1,105 @@
 <template>
-  <div class="rectangle" v-if="displayLinesAsArray.length > 0">
-    <div class="metro-line-logo">
-      <template
-        v-for="(lineName, index) in displayLinesAsArray"
-        :key="lineName"
-      >
-        <!-- Render the component -->
-        <component :is="lineComponents[lineName]" />
-
-        <!-- Arrow between components -->
-        <svg
-          v-if="index < displayLinesAsArray.length - 1"
-          xmlns="http://www.w3.org/2000/svg"
-          width="12"
-          height="20"
-          viewBox="0 0 12 20"
-          fill="none"
-        >
-          <path
-            d="M1.11279 19L10.2059 10L1.11279 1"
-            stroke="black"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </template>
+  <div
+    class="rectangle"
+    v-if="displayLinesAsArray.length > 0"
+    @click="showDetails = !showDetails"
+  >
+    <div class="show-details" v-if="showDetails">
+      <div v-for="element in displayDijkstraPathName" :key="element" class="li">
+        <ul>
+            <li>
+                {{ element }}
+            </li>
+        </ul>
+      </div>
     </div>
-    <div class="footer">
-      <div class="svg-price">
-        <div class="price-svg">
+    <div v-else>
+      <div class="metro-line-logo">
+        <template
+          v-for="(lineName, index) in displayLinesAsArray"
+          :key="lineName"
+        >
+          <!-- Render the component -->
+          <component :is="lineComponents[lineName]" />
+
+          <!-- Arrow between components -->
           <svg
+            v-if="index < displayLinesAsArray.length - 1"
             xmlns="http://www.w3.org/2000/svg"
-            width="35"
-            height="35"
-            viewBox="0 0 35 35"
+            width="12"
+            height="20"
+            viewBox="0 0 12 20"
             fill="none"
           >
             <path
-              d="M6.5625 11.6667L21.9917 4.2875C22.1666 4.20379 22.3564 4.15579 22.5501 4.14633C22.7438 4.13687 22.9374 4.16613 23.1196 4.23239C23.3019 4.29866 23.4691 4.40061 23.6114 4.53227C23.7538 4.66393 23.8685 4.82266 23.9488 4.99917L26.9792 11.6667"
-              stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M8.75 14.5834V11.6667"
-              stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M8.75 20.4167V21.875"
-              stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M8.75 27.7083V30.625"
-              stroke="black"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M29.1665 11.6667H5.83317C4.22234 11.6667 2.9165 12.9725 2.9165 14.5834V27.7084C2.9165 29.3192 4.22234 30.625 5.83317 30.625H29.1665C30.7773 30.625 32.0832 29.3192 32.0832 27.7084V14.5834C32.0832 12.9725 30.7773 11.6667 29.1665 11.6667Z"
+              d="M1.11279 19L10.2059 10L1.11279 1"
               stroke="black"
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
             />
           </svg>
-        </div>
-        <div class="ticket-price">
-          <h4>2.50€</h4>
-        </div>
+        </template>
       </div>
-      <div class="right-side">
-        <div class="duration-min">
-          <div class="duration">
-            {{ displayDijkstraDuration }}
+      <div class="footer">
+        <div class="svg-price">
+          <div class="price-svg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="35"
+              height="35"
+              viewBox="0 0 35 35"
+              fill="none"
+            >
+              <path
+                d="M6.5625 11.6667L21.9917 4.2875C22.1666 4.20379 22.3564 4.15579 22.5501 4.14633C22.7438 4.13687 22.9374 4.16613 23.1196 4.23239C23.3019 4.29866 23.4691 4.40061 23.6114 4.53227C23.7538 4.66393 23.8685 4.82266 23.9488 4.99917L26.9792 11.6667"
+                stroke="black"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M8.75 14.5834V11.6667"
+                stroke="black"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M8.75 20.4167V21.875"
+                stroke="black"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M8.75 27.7083V30.625"
+                stroke="black"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M29.1665 11.6667H5.83317C4.22234 11.6667 2.9165 12.9725 2.9165 14.5834V27.7084C2.9165 29.3192 4.22234 30.625 5.83317 30.625H29.1665C30.7773 30.625 32.0832 29.3192 32.0832 27.7084V14.5834C32.0832 12.9725 30.7773 11.6667 29.1665 11.6667Z"
+                stroke="black"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
           </div>
-          <div class="min">
-            <h4>min</h4>
+          <div class="ticket-price">
+            <h4>2.50€</h4>
+          </div>
+        </div>
+        <div class="right-side">
+          <div class="duration-min">
+            <div class="duration">
+              {{ displayDijkstraDuration }}
+            </div>
+            <div class="min">
+              <h4>min</h4>
+            </div>
           </div>
         </div>
       </div>
@@ -141,6 +156,7 @@ const displayDijkstraPathName = ref([]);
 const displayDijkstraPathLine = ref([]);
 const displaySetDijkstraPathLine = ref(new Set());
 const displayDijkstraDuration = ref(0);
+const showDetails = ref(false);
 
 // Convert Set to array for rendering
 const displayLinesAsArray = computed(() =>
@@ -212,7 +228,7 @@ watch(
   justify-content: center;
   align-items: center;
   margin-left: 10px;
-    margin-top: 5px;
+  margin-top: 5px;
 }
 
 .footer {
@@ -235,12 +251,32 @@ watch(
 }
 
 .duration-min {
-    display: flex;
-    margin-left: 19rem;
+  display: flex;
+  margin-left: 19rem;
 }
 
 .right-side {
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.li {
+    font-family: 'Open Sans';
+    font-size: small;
+    font-weight: lighter;
+}
+
+ul li {
+    margin-bottom: 1px;
+    height: 20px;
+}
+
+ul {
+    list-style-type: disc;
+}
+
+.show-details {
+    margin-top: -20px;
+    padding-bottom: 20px;
 }
 </style>
